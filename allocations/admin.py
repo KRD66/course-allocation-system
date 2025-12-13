e59f8e305a59edb01c2b0d4dcc41424351991c38
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-12-13 17:11:13 +0100

    admin created

--- allocations/admin.py
+++ allocations/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Department, Course, Enrollment
 
-# Register your models here.
+# Custom User admin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'full_name', 'email', 'role', 'is_staff')
+    list_filter = ('role',)
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('full_name', 'role')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Info', {'fields': ('full_name', 'role')}),
+    )
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+    search_fields = ('name', 'code')
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title', 'department', 'lecturer', 'capacity', 'enrolled_count', 'available_slots')
+    list_filter = ('department',)
+    search_fields = ('code', 'title')
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'status', 'requested_at')
+    list_filter = ('status', 'course__department')
+    search_fields = ('student__username', 'course__code')
