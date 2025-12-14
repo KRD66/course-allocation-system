df1aec12521088b4fd828e662688f863a0de49f0
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-12-14 01:45:37 +0100

    path for custom login view

--- allocations/urls.py
+++ allocations/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import CustomLoginView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('', views.home, name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
